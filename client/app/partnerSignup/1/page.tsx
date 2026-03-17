ab7121f6dd66d76ab269daf0f61419280c4edea7
Abhishek V Gopal <abhishekvgopal3@gmail.com>
2026-03-17 10:17:19 +0530

    partner profile fix

--- client/app/partnerSignup/1/page.tsx
+++ client/app/partnerSignup/1/page.tsx
@@ -154,7 +154,11 @@ const ProfileForm = () => {
         partner: {
           address: formData.address,
           avatar: formData.profileImage || "https://fastly.picsum.photos/id/535/200/300.jpg?hmac=iN2CqXJjjbBwtMlTUpWyZV6xFRfk_-XSDYRSk2eFbsQ",
-          identityProof: formData.uploadId
+        },
+        identityProof: {
+            proof_type: formData.idProof,
+            proof_number: formData.uploadId.split('/').pop() || '', // Extract filename from path
+            proof_url: formData.uploadId
         }
       };
 
